8b9fd827325f4af432c0cb48ec02399be31d058e
JapnitS <57968393+JapnitS@users.noreply.github.com>
2021-09-22 22:39:21 -0400

    not working weather

--- server/send-sms.js
+++ server/send-sms.js
@@ -4,12 +4,19 @@ dotenv.config();
 const accountSid = process.env.SID;
 const token = process.env.token;
 const client = require('twilio')(accountSid,token);
-
+const participants = [ '+14168430961'];
+const sendSmsToParticipants  = participants => {
+for(let i =0; i < participants.length; i ++){
 client.messages.create(
-    {
-        to : '+14168430961',
+   
+ {
+        to : participants[i],
         from : '+12898152609',
-        body :'hello from Japnit Singh',
-        mediaUrl : 'https://demo.twilio.com/owl.png'
+        body :'Hello your No-Internet Buddy welcomes you!! PRESS and SEND 1. Search up on google, 2. Read your new emails, 3.Check weather of your city, 4. Request an Uber Ride,',
+        // mediaUrl : 'https://demo.twilio.com/owl.png'
     }
-).then((res) => console.log(res))
+)
+}
+}
+sendSmsToParticipants(participants);
+module.exports = {sendSmsToParticipants};
--- server/server.js
+++ server/server.js
@@ -1,27 +1,84 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const http = require('http');
+// const fetch = require('node-fetch')
+// global.fetch = fetch;
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
+const { sendSmsToParticipants } = require('./send-sms')
+const axios = require('axios')
 
-
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log('hI');
 app.post('/sms', (req, res) => {
-    console.log(req.body);
-    
+    //console.log(req.body);
+    var Body = req.body.Body
     const twiml = new MessagingResponse();
-    var EmailResponse = Emailresponse();
-    twiml.message(EmailResponse);
+  
+    if (Body.startsWith('1')){
+      
+        
+        query = Body.substring(1,Body.length)
+        //twiml.message(googleRequest(query))
+        twiml.message(googleRequest(query))
+    }
+    else if (Body.startsWith('3')){
+
+      query = Body.substring(1,Body.length)
+      let a = checkWeather(query)
+      
+      
+      twiml.message(checkWeather(query))
+
+      
+
+    }
+
     
+   
+    // const weatherdetails = checkWeather(req.body.Body)
+    // console.log(weatherdetails.main)
+   
+    // twiml.message(weatherdetails['main'])
+   
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
+  
+  
   res.end(twiml.toString());
 });
-function Emailresponse(){
-    return ('hello how are you')
+
+const googleRequest = query => {
+  var xmlHttp = new XMLHttpRequest();
+ //google request query
+  xmlHttp.open( "GET", 'https://www.google.com/search?q='+query) 
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+
+
 }
 http.createServer(app).listen(1337, () => {
     console.log('Express server listening on port 1337');
   });
+
+
+
+const checkWeather = city => {
+  // fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=0c4cafc732d5cca1dafebe44159f496d')
+  // .then(data => {
+  //   return data.json();
+  //   })
+var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=0c4cafc732d5cca1dafebe44159f496d', false ); // false for synchronous request
+    
+    //res = xmlHttp.responseText
+    //console.log(JSON.parse(res)["Data"],"loool")
+    // xmlHttp.send( null );
+    // console.log(res)
+    return xmlHttp.responseText;
+    // return xmlHttp.responseText;
+
+}
+
